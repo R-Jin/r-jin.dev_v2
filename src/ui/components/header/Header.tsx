a086aff23ad253fd6935b876fff0bd48f763524a
Ryan Jin <ryanjin007@gmail.com>
2024-01-04 21:41:00 +0100

    Added darkmode toggle to header

--- src/ui/components/header/Header.tsx
+++ src/ui/components/header/Header.tsx
@@ -1,9 +1,13 @@
+import DarkModeToggle from "./DarkModeToggle";
 import NavLinks from "./NavLinks";
 
 export default function Header() {
   return (
-    <header>
-      <NavLinks />
+    <header className="3xl:px-[640px] sticky top-0 z-10 bg-background px-8 transition-colors duration-200 ease-linear md:px-[150px] lg:px-[250px] xl:px-[375px] 2xl:px-[400px]">
+      <div className="flex flex-row items-center justify-between">
+        <NavLinks />
+        <DarkModeToggle />
+      </div>
       <hr />
     </header>
   );
