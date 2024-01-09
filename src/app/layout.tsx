dbe83958dde813b0b4d335e4b84d600a3afba4ac
R-Jin <ryanjin007@gmail.com>
2024-01-09 21:05:50 +0100

    Change background color so it is same over site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { inter, merriweather } from "@/ui/fonts";
 import "@/ui/globals.css";
 
 import Header from "@/ui/components/header/Header";
 import Providers from "@/ui/components/Providers";
+
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function RootLayout({
   children,
@@ -17,9 +20,11 @@ export default function RootLayout({
     >
       <body>
         <Providers>
-          <Header />
-          <main className="bg-background">{children}</main>
-          {/* footer */}
+          <div className="bg-background transition-colors duration-200 ease-linear">
+            <Header />
+            <AnimatePresence>{children}</AnimatePresence>
+            {/* footer */}
+          </div>
         </Providers>
       </body>
     </html>
