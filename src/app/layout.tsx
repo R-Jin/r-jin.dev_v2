8b26d692aa673ec6af1e4bc94d069f3916502f46
R-Jin <ryanjin007@gmail.com>
2024-01-09 23:13:40 +0100

    Animate in the main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,21 @@ import Header from "@/ui/components/header/Header";
 import Providers from "@/ui/components/Providers";
 
 import { AnimatePresence, motion } from "framer-motion";
+import Footer from "@/ui/components/footer/Footer";
+import { usePathname } from "next/navigation";
+
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  // exitState: { opacity: 0, x: 0, y: -50 },
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
   return (
     <html
       lang="en"
@@ -22,6 +31,18 @@ export default function RootLayout({
         <Providers>
           <div className="bg-background transition-colors duration-200 ease-linear">
             <Header />
+            <motion.main
+              key={pathname}
+              initial="hidden"
+              animate="enter"
+              variants={variants}
+              transition={{
+                type: "linear",
+                duration: 0.2,
+              }}
+            >
+              {children}
+            </motion.main>
             <Footer />
           </div>
         </Providers>
