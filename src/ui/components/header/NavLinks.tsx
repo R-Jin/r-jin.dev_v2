693aa06d7e77678ccbcc73f9b6e51d26da06f4ad
R-Jin <ryanjin007@gmail.com>
2024-01-09 20:18:52 +0100

    Add active page indicator and animate the indicator
    Animation plays during page switch and is responsive to window resize.

--- src/ui/components/header/NavLinks.tsx
+++ src/ui/components/header/NavLinks.tsx
@@ -1,13 +1,10 @@
 "use client";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
-type link = {
-  name: string;
-  href: string;
-};
-
-const links: link[] = [
+const links = [
   { name: "Home", href: "/" },
   { name: "Projects", href: "/projects" },
   { name: "Writing", href: "/writing" },
@@ -15,19 +12,49 @@ const links: link[] = [
 ];
 
 export default function NavLinks() {
-  const pathname = usePathname();
+  const [activeWidth, setActiveWidth] = useState(0);
+  const [offsetLeft, setOffsetLeft] = useState(0);
+  const pathname = '/' + usePathname().split('/')[1];
+
+  useEffect(() => {
+    const handleResize = () => {
+      const activeLink = document.querySelector('.active');
+      const nav = document.querySelector('nav');
+      if (activeLink && nav) {
+        setActiveWidth(activeLink.getBoundingClientRect().width);
+        setOffsetLeft(activeLink.getBoundingClientRect().left - nav.getBoundingClientRect().left);
+      }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [pathname, activeWidth])
+
+  let animate = {
+    width: activeWidth,
+    x: offsetLeft,
+    transition: {
+      ease: [.45,.68,.17,.98],
+    }
+  }
 
   return (
-    <nav className="justify-left flex space-x-5 py-8 text-center font-sans text-xs font-semibold text-foreground md:text-xl lg:space-x-10 lg:space-y-0">
-      {links.map((link) => (
-        <Link
-          key={link.name}
-          href={link.href}
-          className={`h-1 bg-accent after:w-0 hover:after:w-full`}
-        >
-          {link.name}
-        </Link>
-      ))}
-    </nav>
+    <div className="relative">
+      <nav className="relative justify-left flex space-x-5 py-8 text-center font-sans text-xs font-semibold text-foreground md:text-xl lg:space-x-10 lg:space-y-0">
+        {links.map((link) => (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={`${pathname === link.href ? "active" : ""}`}
+          >
+            {link.name}
+          </Link>
+        ))}
+      </nav>
+      <motion.span animate={animate} className={`absolute h-[2px] bg-accent block bottom-7 lg:h-1`}/>
+    </div>
   );
 }
